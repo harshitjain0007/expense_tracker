34c25cdf8c5c5060f5b87d7db05db9b633bd74a1
harshitjain0007 <harshit.jain0007@gmail.com>
2025-09-09 18:39:37 +0530

    expense tracker almost done

---
+++ expense.js
@@ -0,0 +1,48 @@
+let count = 0;
+let form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  let amount = document.getElementById("expenseAmount").value;
+  let detail = document.getElementById("expenseDetail").value;
+  let category = document.getElementById("expenseCategory").value;
+
+if (amount === "" || detail === "" || category === ""){
+    alert("Empty fields not allowed");
+}
+else{
+    count++;
+
+  let tbody = document.querySelector("tbody");
+  let row = tbody.insertRow();
+  row.insertCell(0).innerText = count;
+  row.insertCell(1).innerText = "Rs" + amount;
+  row.insertCell(2).innerText = detail;
+  row.insertCell(3).innerText = category;
+  row.insertCell(4).innerText = new Date().toLocaleDateString();
+
+  document.getElementById("expenseAmount").value = "";
+  document.getElementById("expenseDetail").value = "";
+  document.getElementById("expenseCategory").value = "";
+}
+});
+
+let filterdata = document.getElementById("filterByCategory");
+
+filterdata.addEventListener("change", function(){
+let selectcategory = this.value;
+let rowdata = document.querySelectorAll("tbody tr")
+
+rowdata.forEach(function(row){
+let rowcategory = row.cells[3].innerText;
+
+if (selectcategory === "" || rowcategory=== selectcategory){
+
+row.style.display = "";}
+else {
+  row.style.display = "none";
+
+}
+
+}
+})
